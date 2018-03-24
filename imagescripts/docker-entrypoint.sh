4ccda20b3b89e3db23ec7fb1c60319a82c2714b3
Steffen Bleul <sbl@blacklabelops.com>
2018-03-24 10:10:17 +0100

    Custom logging file for image.

--- imagescripts/docker-entrypoint.sh
+++ imagescripts/docker-entrypoint.sh
@@ -4,194 +4,11 @@ set -o errexit
 
 [[ ${DEBUG} == true ]] && set -x
 
-function pipeEnvironmentVariables() {
-  local environmentfile="/etc/profile.d/cronium.sh"
-  cat > ${environmentfile} <<_EOF_
-  #!/bin/sh
-_EOF_
-  sh -c export >> ${environmentfile}
-  sed -i.bak '/^export [a-zA-Z0-9_]*:/d' ${environmentfile}
-}
-
-function createCustomUser {
-    local userid=$1
-    local groupid=$2
-    if [ -n "${groupid}" ]; then
-        addgroup -g $groupid cronium
-    else
-        addgroup cronium
-    fi
-    adduser -G cronium -s /bin/bash -u $userid -h /home/cronium -S cronium
-    export HOME=/home/cronium
-}
-
-function printUserInfo {
-    local userid=$(id -u cronium)
-    local groupid=$(id -g cronium)
-    echo "Starting user with Group-Id: $groupid"
-    echo "Starting user with User-Id: $userid"
-}
-
-function createEnvs {
-    local configFile=$1
-    local job=$2
-    local VAR_TEST="${job}ENV1KEY"
-    local i=1
-    if [ -n "${!VAR_TEST}" ]; then
-        cat >> $configFile <<_EOF_
-    environments:
-_EOF_
-    fi
-    for (( ; ; i++ ))
-    do
-        local VAR_ENV_KEY="${job}ENV${i}KEY"
-        local VAR_ENV_VALUE="${job}ENV${i}VALUE"
-        if [ ! -n "${!VAR_ENV_KEY}" ]; then
-            break
-        fi
-        cat >> $configFile <<_EOF_
-        ${!VAR_ENV_KEY}: ${!VAR_ENV_VALUE}
-_EOF_
-    done
-}
-
-function createJobs {
-    local configFile=$1
-    local i=1
-    local VAR_JOB_NAME="JOB1NAME"
-    if [ -n "${!VAR_JOB_NAME}" ]; then
-        cat >> $configFile <<_EOF_
-  jobs:
-_EOF_
-    fi
-    for (( ; ; i++ ))
-    do
-        local VAR_JOB_NAME="JOB${i}NAME"
-        local VAR_JOB_CRON="JOB${i}CRON"
-        local VAR_JOB_COMMAND="JOB${i}COMMAND"
-        local VAR_JOB_PRE_COMMAND="JOB${i}PRE_COMMAND"
-        local VAR_JOB_POST_COMMAND="JOB${i}POST_COMMAND"
-        local VAR_JOB_SHELLCOMMAND="JOB${i}SHELLCOMMAND"
-        local VAR_JOB_WORKDIR="JOB${i}WORKDIR"
-        local VAR_JOB_ON_ERROR="JOB${i}ON_ERROR"
-        local VAR_JOB_EXECUTION="JOB${i}EXECUTION"
-        local VAR_JOB_TIMEOUT_MINUTES="JOB${i}TIMEOUT_MINUTES"
-
-        if [ ! -n "${!VAR_JOB_NAME}" ]; then
-            break
-        fi
-        cat >> $configFile <<_EOF_
-    - name: ${!VAR_JOB_NAME}
-      cron: '${!VAR_JOB_CRON}'
-      command: ${!VAR_JOB_COMMAND}
-_EOF_
-
-      local jobWorkingDir=
-      if [ -n "${!VAR_JOB_WORKDIR}" ]; then
-        jobWorkingDir=${!VAR_JOB_WORKDIR}
-      elif [ -n "${CRONIUM_WORKING_DIRECTORY}" ]; then
-        jobWorkingDir=${CRONIUM_WORKING_DIRECTORY}
-      fi
-
-      if [ -n "${jobWorkingDir}" ]; then
-        cat >> $configFile <<_EOF_
-      workingDirectory: ${jobWorkingDir}
-_EOF_
-      fi
-
-      local jobShellCommand=
-      if [ -n "${!VAR_JOB_SHELLCOMMAND}" ]; then
-        jobShellCommand=${!VAR_JOB_SHELLCOMMAND}
-      elif [ -n "${CRONIUM_WORKING_DIRECTORY}" ]; then
-        jobShellCommand=${CRONIUM_WORKING_DIRECTORY}
-      else
-        jobShellCommand="/bin/bash -c"
-      fi
-
-      if [ -n "${jobShellCommand}" ]; then
-        cat >> $configFile <<_EOF_
-      shellCommand: ${jobShellCommand}
-_EOF_
-      fi
-
-      local jobOnError=
-      if [ -n "${!VAR_JOB_ON_ERROR}" ]; then
-        jobOnError=${!VAR_JOB_ON_ERROR}
-      elif [ -n "${CRONIUM_ON_ERROR}" ]; then
-        jobOnError=${CRONIUM_ON_ERROR}
-      else
-        jobOnError="continue"
-      fi
-
-      if [ -n "${jobOnError}" ]; then
-        cat >> $configFile <<_EOF_
-      errorMode: ${jobOnError}
-_EOF_
-      fi
-
-      local jobExecution=
-      if [ -n "${!VAR_JOB_EXECUTION}" ]; then
-        jobExecution=${!VAR_JOB_EXECUTION}
-      elif [ -n "${CRONIUM_EXECUTION}" ]; then
-        jobExecution=${CRONIUM_EXECUTION}
-      else
-        jobExecution="sequential"
-      fi
-
-      if [ -n "${jobExecution}" ]; then
-        cat >> $configFile <<_EOF_
-      execution: ${jobExecution}
-_EOF_
-      fi
-
-      local preCommand=
-      if [ -n "${!VAR_JOB_PRE_COMMAND}" ]; then
-        preCommand=${!VAR_JOB_PRE_COMMAND}
-      fi
-
-      if [ -n "${preCommand}" ]; then
-        cat >> $configFile <<_EOF_
-      preCommand: ${preCommand}
-_EOF_
-      fi
-
-      local postCommand=
-      if [ -n "${!VAR_JOB_POST_COMMAND}" ]; then
-        postCommand=${!VAR_JOB_POST_COMMAND}
-      fi
-
-      if [ -n "${postCommand}" ]; then
-        cat >> $configFile <<_EOF_
-      postCommand: ${postCommand}
-_EOF_
-      fi
-
-      local timeoutMinutes=
-      if [ -n "${!VAR_JOB_TIMEOUT_MINUTES}" ]; then
-        timeoutMinutes=${!VAR_JOB_TIMEOUT_MINUTES}
-      fi
-
-      if [ -n "${timeoutMinutes}" ]; then
-        cat >> $configFile <<_EOF_
-      timeOutMinutes: ${timeoutMinutes}
-_EOF_
-      fi
-
-      createEnvs $configFile JOB${i}
-    done
-}
-
-function createConfig {
-    touch application.yml
-    cat > application.yml <<_EOF_
----
-
-crow:
-_EOF_
-    createJobs application.yml
-}
+source ${CROW_HOME}/create-config.sh
 
 source ${CROW_HOME}/jobber-migration.sh
+
+source $CROW_HOME/import-gpg.sh
 
 if [ ! -e "application.yml" ]; then
   createConfig
@@ -199,22 +16,27 @@ fi
 
 if [ -n "${CRONIUM_UID}" ]; then
   createCustomUser $CRONIUM_UID $CRONIUM_GID
-  #chown crow:crow application.yml
 fi
 
 if [ "${DEBUG}" = 'true' ]; then
   cat application.yml
 fi
 
-source $CROW_HOME/import-gpg.sh
+log4j_config=${CROW_HOME}/log4j2-spring.xml
+if [ -n "${LOG4J_CONFIG}" ]; then
+  log4j_config=${LOG4J_CONFIG}
+fi
+
+importKeys
 
 if [ "$1" = 'cronium' ] || [ "${1:0:1}" = '-' ]; then
+  migrateJobberEnvs
   pipeEnvironmentVariables
   if [ -n "${CROW_UID}" ]; then
       printUserInfo
-      exec su-exec cronium java -jar ${CROW_HOME}/crow-application.jar "$@"
+      exec su-exec cronium java -Dlogging.config=${log4j_config} -jar ${CROW_HOME}/crow-application.jar "$@"
   else
-    exec java -jar ${CROW_HOME}/crow-application.jar "$@"
+    exec java -Dlogging.config=${log4j_config} -jar ${CROW_HOME}/crow-application.jar "$@"
   fi
 else
   if [ -n "${CROW_UID}" ]; then
--- imagescripts/jobber-migration.sh
+++ imagescripts/jobber-migration.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 
+function migrateJobberEnvs() {
+  for (( i = 1; ; i++ ))
+  do
+    local VAR_JOB_ON_ERROR="JOB_ON_ERROR$i"
+    local VAR_JOB_NAME="JOB_NAME$i"
+    local VAR_JOB_COMMAND="JOB_COMMAND$i"
+    local VAR_JOB_TIME="JOB_TIME$i"
+    local VAR_JOB_NOTIFY_ERR="JOB_NOTIFY_ERR$i"
+    local VAR_JOB_NOTIFY_FAIL="JOB_NOTIFY_FAIL$i"
 
-for (( i = 1; ; i++ ))
-do
-  VAR_JOB_ON_ERROR="JOB_ON_ERROR$i"
-  VAR_JOB_NAME="JOB_NAME$i"
-  VAR_JOB_COMMAND="JOB_COMMAND$i"
-  VAR_JOB_TIME="JOB_TIME$i"
-  VAR_JOB_NOTIFY_ERR="JOB_NOTIFY_ERR$i"
-  VAR_JOB_NOTIFY_FAIL="JOB_NOTIFY_FAIL$i"
+    if [ ! -n "${!VAR_JOB_NAME}" ]; then
+      break
+    fi
 
-  if [ ! -n "${!VAR_JOB_NAME}" ]; then
-    break
-  fi
+    local migratedCron=$(echo "${!VAR_JOB_TIME}" | awk '{ $1=""; print}')
 
-  migratedCron=$(echo "${!VAR_JOB_TIME}" | awk '{ $1=""; print}')
+    export JOB${i}NAME=${!VAR_JOB_NAME}
+    export JOB${i}CRON="$migratedCron"
+    export JOB${i}COMMAND="${!VAR_JOB_COMMAND}"
 
-  echo $migratedCron
-
-  export JOB${i}NAME=${!VAR_JOB_NAME}
-  export JOB${i}CRON="$migratedCron"
-  export JOB${i}COMMAND="${!VAR_JOB_COMMAND}"
-
-  case "${!VAR_JOB_ON_ERROR}" in
-    Continue)
-      export JOB${i}ON_ERROR=continue
-      ;;
-    Stop)
-      export JOB${i}ON_ERROR=stop
-      ;;
-    *)
-      export JOB${i}ON_ERROR=continue
-      ;;
-  esac
-done
+    case "${!VAR_JOB_ON_ERROR}" in
+      Continue)
+        export JOB${i}ON_ERROR=continue
+        ;;
+      Stop)
+        export JOB${i}ON_ERROR=stop
+        ;;
+      *)
+        export JOB${i}ON_ERROR=continue
+        ;;
+    esac
+  done
+}
